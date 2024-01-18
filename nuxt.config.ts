23c0eeffa41c7553f326377305dbdb66df87ccdb
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2024-01-18 12:41:19 +0100

    Added meta image to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+// import MetaImage from './assets/images/intro.jpg'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -5,7 +7,8 @@ export default defineNuxtConfig({
     head: {
         title: 'Portfolio | Patrick Huizinga',
         meta: [
-          { name: 'description', content: 'A front-end developer that loves doing creative things with code.' }
+          { name: 'description', content: 'A front-end developer that loves doing creative things with code.' },
+          { name: 'og:image', content: 'https://patrickhuizinga.nl/intro.jpg' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
