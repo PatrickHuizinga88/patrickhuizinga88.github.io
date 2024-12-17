b01392ed2001057c06ba5b96e3d0e079d62817b3
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2024-12-17 14:50:01 +0100

    Updated dependencies Updated about me description Added new item to My adventure section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -54,10 +54,10 @@ export default {
 		},
 	},
 	safelist: [
-		{ pattern: /from-(primary|secondary|white|neutral|teal|rose)-(200|300|500|950)/ },
-		{ pattern: /via-(primary|secondary|white|neutral|teal|rose)-(200|300|500|950)/ },
-		{ pattern: /to-(primary|secondary|white|neutral|teal|rose)-(200|300|500|950)/ },
-		{ pattern: /bg-(primary|secondary|white|neutral|teal|rose)-(200|300|500|950)/ },
+		{ pattern: /from-(primary|secondary|white|neutral|teal|rose|indigo)-(200|300|500|950)/ },
+		{ pattern: /via-(primary|secondary|white|neutral|teal|rose|indigo)-(200|300|500|950)/ },
+		{ pattern: /to-(primary|secondary|white|neutral|teal|rose|indigo)-(200|300|500|950)/ },
+		{ pattern: /bg-(primary|secondary|white|neutral|teal|rose|indigo)-(200|300|500|950)/ },
 	],
 	plugins: [
 		require("tailwindcss-animate"),
