a02739f61d4da47906e4a1b3b75c717cd6547aff
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2025-04-05 10:14:32 +0200

    Updated dependencies Added DietTailor to showcase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://patrickhuizinga.nl',
-  integrations: [tailwind(), vue(), sitemap()]
+  integrations: [vue(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
